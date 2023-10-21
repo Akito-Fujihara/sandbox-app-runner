9c6fc264f7f7a1a40873a4d225f802cfc1884b95
Akito-Fujihara <1998922akito@gmail.com>
2023-10-21 20:56:19 +0900

    app runner では ingress を設定できないので修正

--- sg.tf
+++ sg.tf
@@ -3,10 +3,10 @@ resource "aws_security_group" "app_runner" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.ip_address]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
