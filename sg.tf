92fe8114eda162eacb82dfdceff8e8e0026ddd70
Akito-Fujihara <1998922akito@gmail.com>
2023-10-21 19:43:02 +0900

    SG の作成

---
+++ sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "app" {
+  name   = var.name
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.ip_address]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -21,3 +21,7 @@ variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
+
+variable "ip_address" {
+  type = string
+}
